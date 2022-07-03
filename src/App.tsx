f7e941fb569b92a7f1b0343e2cd59f006d2c1182
Henry Tran <henry.q.tran@shopify.com>
2022-07-02 18:06:43 -0700

    test add typescript component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>My Chores</code> My Core Web Apps.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nahh
-        </a>
-      </header>
+      <h1>My Chores</h1>
     </div>
   );
 }
