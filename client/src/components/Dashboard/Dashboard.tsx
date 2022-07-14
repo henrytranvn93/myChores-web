0c206d68d5670846fad5fa8285c1ef6033c183b6
Henry Tran <henry.q.tran@shopify.com>
2022-07-13 20:50:36 -0700

    create task and test data

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Task from '../Task/Task'
 
 type Props = {}
 
 export default function Dashboard({}: Props) {
+  const testList = [{
+    isDone: false,
+    description: "do clothes",
+    type: "routine",
+    assignee: "Henry",
+    reward: 500,
+    id: 2125
+  },
+  {
+    isDone: false,
+    description: "do dishes",
+    type: "routine",
+    assignee: "Katy",
+    reward: 600,
+    id: 2125
+  }]
+
+  const [tasks, setTasks] = useState(testList)
+
   return (
+    <>
     <div>Dashboard with members in house with tasks</div>
+    {tasks.map(task => 
+      <Task/>
+    )}
+    </>
   )
 }
